9c5ba12925bfa06316c3b3a5def1028ab1e542c2
ashNOLOGY <mosno@mosno.net>
2018-07-10 22:14:42 -0400

    Chapter 2 - Trivia Game
    This is the first upload.
    All it has is some general data and the Psudocode comments for us to start filling them in.
    Make sure to add your name to the "Coded by" section at the top.
    Lets DO this.

---
+++ triviaGame.py
@@ -0,0 +1,31 @@
+'''
+Chapter Two
+Useless Trivia Game
+Coded by ashNOLOGY
+GitHub w/ Pi50DD
+'''
+
+#Display Name of the game.
+
+
+#Get input from the user
+#User's Name
+
+#User's Age (convert to an int)
+
+#User's Wight (convert to an int)
+
+
+
+#The manipulations to the information
+#Print the name in Uppercase & Lowercase
+
+#Print name 5 time ( called = name * 5)
+
+#Calculate seconds (seconds = age * 365 * 24 * 60 * 60
+
+#Calculate Moon Wieght ( m_w = weight / 6)
+
+#Calculate Sun Weight ( s_w = weight * 27.1)
+
+#Await user input to close program
