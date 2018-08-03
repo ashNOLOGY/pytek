453717c506d76c35c4f688377c299a25342d0154
ashNOLOGY <28209344+ashNOLOGY@users.noreply.github.com>
2018-08-03 12:56:39 -0400

    WORDS BACK
    A simple program that asks the user for a word, and then it displays it backwards

---
+++ Chapter_4/ash_ch4_WordsBack-v1.py
@@ -0,0 +1,43 @@
+'''
+NCC
+CODE TESTER
+
+Project: PyTek
+Code by: ashNOLOGY
+
+'''
+
+
+
+
+#------ The Imports
+import math
+import random
+import os
+
+
+# intro
+print("\n\t\t\t---------------"
+      "\n\t\t\t|  WORDS BACK  | "
+      "\n\t\t\t---------------\n")
+
+#--- Ask user to enter a word
+word = input("Enter a word: ")
+
+#-- print it out backwards
+print("Your word backwards is: ", word[::-1])
+
+
+
+
+
+
+
+
+
+
+
+# outro
+input("\n\t\t\t-----------------"
+      "\n\t\t\t| ENTER to Exit |"
+      "\n\t\t\t-----------------")
